28b1b13c0a65ea0853fc7974d7e22156234d4955
Your Name <you@example.com>
2019-01-19 23:23:42 -0300

    agregando mostrar data

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,57 @@
  * Acá va solo el manejo del DOM
  * 
  */
-window.onload = () => {
-    // Creando data
- const data = (window.DATA.characters);
- //console.log(data)
+ // Creando variable para data de personajes
+ const dataA = (DATA.characters);
+//Creando data episodios
+//const episodes = (window.EPISODES.episodes);
  //llamo a mi root
- const containerRoot = document.getElementById("root");
+ const containerRoot = document.getElementById('root');
 
 
- 
+ //Mostrar data
+ /*const showData = (dataA) => {
+     let result = '';
+     dataA.forEach(element => {
+         result = containerRoot.innerHTML += `
+         <div class="card">
+         <div class="img">
+         <img src="${element.characterImageFull}"> 
+         </div>
+         </div>`
+     });
+     return result;
+ }
+ window.onload = showData(dataA);*/
+
+
+
+//Mostrando la Data
+const showData = (dataA) => {
+    let result = '';
+    characterActors(dataA);
+    return result;
 }
+
+        
+
+//Función para ver personajes
+ function characterActors(dataP){
+    // limpio div
+   containerRoot.innerHTML = '';
+   dataP.forEach(element => {
+       containerRoot.innerHTML += `
+        <div class="card">
+           <div class="box">
+                <div class="img">
+                   <img src="${element.characterImageFull}"></div>
+                   <h2>Name:${element.characterName}<span>Actor: ${element.actorName}</span></h2>
+           </div>
+        </div>`
+    });
+}
+
+
+
+window.onload = showData(dataA);
+//no todos tienen houseName
