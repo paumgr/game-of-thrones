fa97cf74e19ee3e2bf5c26157724596467b40b37
Your Name <you@example.com>
2019-01-20 19:02:33 -0300

    agregando funcion filtrar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,13 @@
  * Acá va solo la lógica
  * 
  */
+//Función para filtrar
+const filterSeason = (dataB, condition) => {
+    //filter es un ciclo iterativo como el for, forech, reviw
+    const filteredSeason= dataB.filter(element => {
+        return element.seasonNum === condition
+    })
+    return filteredSeason;
+  };
+  window.filterSeason=filterSeason;
+  
